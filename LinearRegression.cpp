0470130242c3a2cba76d5480f1d006ff94aaf84f
Przemysław Pająk <office@fearlessspider.com>
2021-12-04 16:10:04 +0100

    add eigen as submodule add header for LinearRegression

---
+++ LinearRegression.cpp
@@ -0,0 +1,5 @@
+//
+// Created by f3ar13ss on 04.12.2021.
+//
+
+#include "LinearRegression.h"
---
+++ LinearRegression.h
@@ -0,0 +1,25 @@
+//
+// Created by f3ar13ss on 04.12.2021.
+//
+
+#ifndef MACHINELEARNINGCPP_LINEARREGRESSION_H
+#define MACHINELEARNINGCPP_LINEARREGRESSION_H
+
+#include "submodules/eigen/Eigen/Dense"
+#include <vector>
+
+using namespace std;
+
+class LinearRegression {
+
+public:
+    LinearRegression()
+    {}
+
+    float OLS_Cost(Eigen::MatrixXd X, Eigen::MatrixXd y, Eigen::MatrixXd theta);
+    std::tuple<Eigen::VectorXd, vector<float>> GradientDescent(Eigen::MatrixXd X, Eigen::MatrixXd y, Eigen::VectorXd theta, float alpha, int iters);
+    float RSquared(Eigen::MatrixXd y, Eigen::MatrixXd y_hat);
+};
+
+
+#endif //MACHINELEARNINGCPP_LINEARREGRESSION_H
